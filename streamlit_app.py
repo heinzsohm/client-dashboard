ff676a64f4c0da6ce56f1552c52dc1f9f2e20555
heinzsohm <113530331+heinzsohm@users.noreply.github.com>
2024-11-27 01:27:00 +0000

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+st.title("Client Portal")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+dict_d = [{'a':100,'b':18,'c':'March'}, {'a':150,'b':28,'c':'April'},{'a':200,'b':10,'c':'May'}]
+df = pd.DataFrame.from_dict(dict_d)
+st.bar_chart(df,x="c",y=["a", "b"])
