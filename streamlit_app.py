306311baa855fc2123fe3bb4b74c02bf0aa4f752
heinzsohm <113530331+heinzsohm@users.noreply.github.com>
2024-11-27 02:39:05 +0000

    data

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.title("Client Portal")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-dict_d = [{'a':100,'b':18,'c':'March'}, {'a':150,'b':28,'c':'April'},{'a':200,'b':10,'c':'May'}]
-df = pd.DataFrame.from_dict(dict_d)
-st.bar_chart(df,x="c",y=["a", "b"])
 
 conn = st.connection("postgresql", type="sql")
 
@@ -16,6 +9,6 @@ conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT * FROM clients;', ttl="10m")
 
 df_contracts = conn.query('SELECT A.*,b.name FROM client_contracts A JOIN clients B on A.client_uuid=B.uuid;', ttl="10m")
-df_payments = conn.query('SELECT a.amount,a.currency,a.payment_date,a.bank_account_identifier,a.uuid,b.name FROM client_payments a JOIN clients b on a.client_uuid = b.uuid;', ttl="10m")
+df_payments = conn.query('SELECT a.amount,a.currency,to_char(a.payment_date,"YYYY-MM") as payment_date,a.bank_account_identifier,a.uuid,b.name FROM client_payments a JOIN clients b on a.client_uuid = b.uuid;', ttl="10m")
 df_payments['usd_amount'] = df_payments.apply(lambda x: x['amount']/4000  if x['currency']=='COP' else x['amount']/20 if x['currency']=='MXN' else x['amount'], axis=1)
 st.bar_chart(df_payments,x="payment_date",y=["usd_amount"])
