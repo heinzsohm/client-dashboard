5f6536e116a5d398cdd275b40e1bb6921601d0ee
heinzsohm <113530331+heinzsohm@users.noreply.github.com>
2024-11-27 01:33:50 +0000

    push

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,12 @@ st.write(
 dict_d = [{'a':100,'b':18,'c':'March'}, {'a':150,'b':28,'c':'April'},{'a':200,'b':10,'c':'May'}]
 df = pd.DataFrame.from_dict(dict_d)
 st.bar_chart(df,x="c",y=["a", "b"])
+
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM clients;', ttl="10m")
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.uuid}:")
