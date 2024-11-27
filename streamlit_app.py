19c43d920e6b392df011b14ca14b3bdb854d4d5d
heinzsohm <113530331+heinzsohm@users.noreply.github.com>
2024-11-27 02:41:26 +0000

    sas

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT * FROM clients;', ttl="10m")
 
 df_contracts = conn.query('SELECT A.*,b.name FROM client_contracts A JOIN clients B on A.client_uuid=B.uuid;', ttl="10m")
-df_payments = conn.query('SELECT a.amount,a.currency,to_char(a.payment_date,"YYYY-MM") as payment_date,a.bank_account_identifier,a.uuid,b.name FROM client_payments a JOIN clients b on a.client_uuid = b.uuid;', ttl="10m")
+df_payments = conn.query("SELECT a.amount,a.currency,TO_CHAR(payment_date,'yyyy-mm')  as payment_date,a.bank_account_identifier,a.uuid,b.name FROM client_payments a JOIN clients b on a.client_uuid = b.uuid;", ttl="10m")
 df_payments['usd_amount'] = df_payments.apply(lambda x: x['amount']/4000  if x['currency']=='COP' else x['amount']/20 if x['currency']=='MXN' else x['amount'], axis=1)
 st.bar_chart(df_payments,x="payment_date",y=["usd_amount"])
