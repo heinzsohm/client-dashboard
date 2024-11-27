a8380158a58d01689a41731635c972b89f8311a6
heinzsohm <113530331+heinzsohm@users.noreply.github.com>
2024-11-27 23:18:59 +0000

    sjaksj

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,4 +69,4 @@ st.dataframe(datefiltered_df, column_order=("payment_date","amount","currency","
 datefiltered_payments = datefiltered_df['usd_amount'].sum()
 st.metric("Total Payments",numerize(datefiltered_payments))
 
-st.markdown(":sunglasses: ## Payment Schedules")
+st.text("#Algo")
