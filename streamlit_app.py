aaf0155d4b8ed21c9abaa4db18f76f801490aa3a
heinzsohm <113530331+heinzsohm@users.noreply.github.com>
2024-12-10 00:39:05 +0000

    skal

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,7 +121,7 @@ st.bar_chart(monthly_counts.set_index('year_month'))
 df_cohorts = conn.query('''WITH cohorts as (
     SELECT client_uuid, min(TO_CHAR(payment_date,'yyyy-mm')) as cohort FROM client_payments GROUP BY 1 
     )
-    SELECT b.cohort,ROUND(SUM(CASE WHEN A.CURRENCY = 'USD' THEN a.amount WHEN a.currency = 'MXN' THEN a.amount/20  WHEN a.currency ='COP' THEN a.amount/4200 ELSE a.AMount END ),2)
+    SELECT b.cohort,ROUND(SUM(CASE WHEN A.CURRENCY = 'USD' THEN a.amount WHEN a.currency = 'MXN' THEN a.amount/20  WHEN a.currency ='COP' THEN a.amount/4200 ELSE a.amount END ),2) as total_amount, count(distinct a.client_uuid) as num_clients
     FROM client_payments a 
     JOIN cohorts b on a.client_uuid = b.client_uuid
     GROUP BY 1 ''', ttl="10m")
